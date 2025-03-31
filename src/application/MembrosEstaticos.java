ef731b9e358f0d130ec8e070b1a6104a6053425e
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-03-31 11:39:25 -0300

    membros estaticos com classe externa

--- src/application/MembrosEstaticos.java
+++ src/application/MembrosEstaticos.java
@@ -2,34 +2,27 @@ package application;
 
 import java.util.Scanner;
 
-public class MembrosEstaticos {
+import util.Calculator;
 
-	public static final double PI = 3.14159;
+public class MembrosEstaticos {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		Calculator calc = new Calculator();
 
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 
-		double v = volume(radius);
+		double v = calc.volume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", PI);
+		System.out.printf("PI value: %.2f%n", calc.PI);
 
 		sc.close();
-	}
-
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
 }
